6c10d21043ff5ecf3ec9433cb6da551a4d43a335
Nawaphon777 <locklove868@gmail.com>
2023-11-03 13:44:19 +0700

    1

---
+++ pages/login.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="login-form">
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input type="text" id="username" v-model="formData.username" required>
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" id="password" v-model="formData.password" required>
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      formData: {
+        username : "",
+        password : ""
+      }
+    };
+  },
+// async created() {
+// console.log("id=", this.$route.query.username);
+// this.username = this.$route.query.username;
+// const url = "http://localhost:7001/list";
+// const res = await fetch(url);
+// const data = await res.json();
+// console.log("data=", data.datas.username);
+// this.username = data.datas.username;
+// this.password = data.datas.password;
+// },
+
+methods: {
+   async login() {
+    console.log("data =>",this.formData)
+    const res = await axios.post("http://localhost:7001/login",this.formData);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.login-form {
+  max-width: 300px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+  text-align: center;
+}
+
+.login-form h2 {
+  margin-bottom: 20px;
+  color: #007bff;
+}
+
+.form-group {
+  margin: 10px 0;
+}
+
+label {
+  display: block;
+  font-weight: bold;
+}
+
+input[type="text"],
+input[type="password"] {
+  width: 100%;
+  padding: 10px;
+  margin: 5px 0;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+button {
+  background-color: #007bff;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:disabled {
+  background-color: #6c757d;
+  cursor: not-allowed;
+}
+
+.message {
+  color: red;
+  font-size: 14px;
+}
+
+/* Add more styles as needed */
+</style>
+
---
+++ pages/login1.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+      <h1>Login</h1>
+      <form @submit.prevent="login">
+        <input type="text" v-model="email" placeholder="Email" required />
+        <input type="password" v-model="password" placeholder="Password" required />
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  </template>
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    async login() {
+      // Send an API request to your server for authentication
+      const userData = {
+        email: this.email,
+        password: this.password,
+      };
+      
+      try {
+        // Use Axios or another HTTP library to send the request
+        const response = await axios.post('/login', userData);
+        
+        // Handle the response here, e.g., check for authentication success
+        if (response.data.authenticated) {
+          // Redirect to a new page or perform other actions
+          this.$router.push('/dashboard');
+        } else {
+          // Show an error message
+          console.error('Authentication failed');
+        }
+      } catch (error) {
+        console.error('An error occurred:', error);
+      }
+    },
+  },
+};
+</script>
+  
